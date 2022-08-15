51da0c34d6d5e7a6ad1902dd9e01ac8fdeaaf432
SG <shoury24gupta@gmail.com>
2022-08-15 13:59:47 +0530

    Happy Independence Day

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-const buttons = document.querySelectorAll('a');
+const button1 = document.getElementById("saffron");
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        let x = e.clientX - e.target.offsetLeft; 
-        let y = e.clientY - e.target.offsetTop; 
-        
-        let ripples = document.createElement('span');
-        ripples.style.left = x + "px";
-        ripples.style.top = y + "px";
+button1.addEventListener('click', function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
 
-        this.appendChild(ripples);
+    let ripples = document.createElement('span');
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
 
-        setTimeout(() => {
-            ripples.remove();
-        }, 1000);
-    })
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+        ripples.remove();
+    }, 1000);
+})
+
+const button2 = document.getElementById("white");
+
+button2.addEventListener('click', function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('code');
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
+
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+        ripples.remove();
+    }, 1000);
+})
+
+const button3 = document.getElementById("green");
+
+button3.addEventListener('click', function (e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('select');
+    ripples.style.left = x + "px";
+    ripples.style.top = y + "px";
+
+    this.appendChild(ripples);
+
+    setTimeout(() => {
+        ripples.remove();
+    }, 1000);
 })
